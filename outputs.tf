68111c5ddfe322d4c15e128137eb4bcaf47e99b8
arunvdsharma <arun.vd.sharma@gmail.com>
2023-05-11 14:54:35 +0530

    Added variable names and output names

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
+output "ec2_instance_ami" {
+  value = aws_instance.web.ami
+}
 
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "ec2_instance_arn" {
+  value = aws_instance.web.arn
+}
