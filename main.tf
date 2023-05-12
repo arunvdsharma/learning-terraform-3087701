b1061068986b133b34609e20aac902026cde6fd1
arunvdsharma <arun.vd.sharma@gmail.com>
2023-05-12 19:03:27 +0530

    new update

--- main.tf
+++ main.tf
@@ -14,8 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
-  default = true
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1", "us-east-2"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "blog_sg" {
@@ -23,13 +37,13 @@ module "blog_sg" {
 
   name        = "blog_web"
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
-  egress_rules[0].name="Outbound"
+
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
