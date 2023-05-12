82b50bb991418fe4255603f251306052c68cf49b
arunvdsharma <arun.vd.sharma@gmail.com>
2023-05-12 10:41:48 +0530

    Created security group

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.ec2_instance_type
-
+  vpc_security_group_ids = [aws_security_group.blog.id]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "allow_tls"
+  description = "Allow https and https in. Allow all outbound"
+  vpc_id      = aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in"{
+    type             = "ingress" 
+    description      = "incoming https"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in"{
+    type             = "ingress" 
+    description      = "incoming https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out"{
+    type             = "egress" 
+    description      = "outbound all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.blog.id
 }
