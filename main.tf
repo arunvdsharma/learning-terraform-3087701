0cd37373ae644826c6b35fbee00879e60710436f
arunvdsharma <arun.vd.sharma@gmail.com>
2023-05-12 18:39:03 +0530

    fixed vpcid

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "blog_sg" {
 
   name        = "blog_web"
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = module.vpc.public_subnets[0].vpc_id
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
